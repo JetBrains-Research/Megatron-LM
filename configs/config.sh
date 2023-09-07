2c81b3a573f4f93b7ece641c2b366d37432f5962
Timur <timur.galimzyanov@jetbrains.com>
2023-09-07 22:22:26 +0200

    Added config files, made bash scripts for basic commands

---
+++ configs/config.sh
@@ -0,0 +1,4 @@
+MEGATRON_PATH="/home/galimzyanov/Megatron-LM"
+DATA_PATH="home/galimzyanov/data/Megatron/dataset_dev"
+CHECKPOINTS_PATH="/home/galimzyanov/data/Megatron/checkpoints_dev"
+VOCAB_FILE="bert-vocab.txt"
