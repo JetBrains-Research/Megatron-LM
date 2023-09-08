c50ff64ee53f61aff74be50e8f70edd795f76c67
Timur <timur.galimzyanov@jetbrains.com>
2023-09-08 15:16:11 +0200

    Added option of HF Tokenizer.

--- configs/config_local.sh
+++ configs/config_local.sh
@@ -1,9 +1,12 @@
 MEGATRON_PATH="/workspace/megatron/"
 DATA_PATH="/workspace/dataset/"
+MODEL_PATH="/workspace/model_setup"
+CHECKPOINT_PATH="/workspace/checkpoints"
 
 # DATA_FILE="data_dev.jsonl"
 DATA_FILE="data_dev_short.jsonl"
 DATA_FILE_PATH="${DATA_PATH}${DATA_FILE}"
 
-VOCAB_FILE="bert-large-uncased-vocab.txt"
-VOCAB_FILE_PATH="${DATA_PATH}${VOCAB_FILE}"
+VOCAB_FILE="${MODEL_PATH}/bert-large-uncased-vocab.txt"
+MERGE_FILE="gpt2-merges.txt"
+## TODO remove VOCAB_FILE as requred parameter if HF tokenizer is used
--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -3,7 +3,7 @@ source /workspace/megatron/configs/config_local.sh
 python "${MEGATRON_PATH}tools/preprocess_data.py" \
        --input ${DATA_FILE_PATH} \
        --output-prefix "${DATA_PATH}dev" \
-       --vocab-file ${VOCAB_FILE_PATH} \
+       --vocab-file ${VOCAB_FILE} \
        --tokenizer-type HFTokenizer \
        --tokenizer-model "Salesforce/codet5p-220m"\
        --workers 8\
